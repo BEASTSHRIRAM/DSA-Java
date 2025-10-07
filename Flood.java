6410fb902cbab526c654989c9db1106f4b9e8a90
shriramthebeast <shrikulk20@gmail.com>
2025-10-07 21:17:29 +0530

    GG

---
+++ Flood.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class Flood {
+    public static void main(String[] args) {
+        int arr[]={1,2,0,0,2,3};
+        System.out.println(Arrays.toString(FloodInCity(arr)));
+        
+    }
+    static int[] FloodInCity(int[] rains){
+        int ans[]=new int[rains.length];
+        HashMap<Integer, Integer> lastd = new HashMap<>();
+        TreeSet<Integer> dd = new TreeSet<>();
+        for(int i=0;i<rains.length;i++){
+            int lakes=rains[i];
+            if(lakes>0){
+                ans[i]=-1;
+                if(lastd.containsKey(lakes)){
+                    int lri=lastd.get(lakes);
+                    Integer ddi=dd.higher(lri);
+                    if(ddi==null){
+                        return new int[]{};
+                    }else{
+                        ans[ddi]=lakes;
+                        dd.remove(ddi);
+                    }
+                }
+                lastd.put(lakes,i);
+            }else if(rains[i]==0){
+                dd.add(i);
+                ans[i]=1;
+                
+            }
+           
+        }
+        return ans;
+    }
+    
+}
---
+++ swiminrisingwater.java
@@ -0,0 +1,29 @@
+import java.util.PriorityQueue;
+
+public class swiminrisingwater {
+    public static void main(String[] args) {
+        
+    }
+    Public int swimInWater(int[][] grid) {
+        int n = grid.length;
+        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        boolean[][] visited=new boolean[n][n];
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        pq.add(new int[]{grid[0][0], 0, 0});
+        while (!pq.isEmpty()) {
+            int[] top=pq.poll();
+            int time=top[0],r=top[1],c=top[2];
+            if (r==n-1 &&c==n-1) return time;
+            if (visited[r][c])continue;
+            visited[r][c]=true
+            for (int[] d : dirs) {
+                int nr = r + d[0], nc = c + d[1];
+                if (nr >= 0 && nc >= 0 && nr < n && nc < n && !visited[nr][nc]) {
+                    pq.add(new int[]{Math.max(time, grid[nr][nc]), nr, nc});
+                }
+            }
+        }
+        return -1;
+    }
+    
+}
