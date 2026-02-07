91355f38d76d9041aafed73c96c85bd937d2c461
shriramthebeast <shrikulk20@gmail.com>
2026-02-07 23:17:26 +0530

    BST

--- Trees/Main.java
+++ Trees/Main.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        BinaryTree tree =new BinaryTree();
-        tree.populate(scanner);
+//        Scanner scanner=new Scanner(System.in);
+//        BinaryTree tree =new BinaryTree();
+//        tree.populate(scanner);
+//        tree.display();
+        BST tree =new BST();
+        int[] nums={2,4,5,6,8,9,10};
+        tree.populateSorted(nums);
         tree.display();
     }
 }
