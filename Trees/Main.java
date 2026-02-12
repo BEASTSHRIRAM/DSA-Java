7482ac0056cf1ccde9e7bcb834cc7f443d346bdc
shriramthebeast <shrikulk20@gmail.com>
2026-02-12 23:39:06 +0530

    AVL

--- Trees/Main.java
+++ Trees/Main.java
@@ -7,10 +7,15 @@ public class Main {
 //        Scanner scanner=new Scanner(System.in);
 //        BinaryTree tree =new BinaryTree();
 //        tree.populate(scanner);
+////        tree.display();
+//        BST tree =new BST();
+//        int[] nums={2,4,5,6,8,9,10};
+//        tree.populateSorted(nums);
 //        tree.display();
-        BST tree =new BST();
-        int[] nums={2,4,5,6,8,9,10};
-        tree.populateSorted(nums);
-        tree.display();
+        AVL tree=new AVL();
+        for(int i=0;i<1000;i++){
+            tree.insert(i);
+        }
+        System.out.println(tree.height());
     }
 }
