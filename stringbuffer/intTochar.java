0e860571d659c56ff762187159233d511c885836
shriramthebeast <shrikulk20@gmail.com>
2026-02-14 19:08:36 +0530

    revised StringBuffer

---
+++ stringbuffer/intTochar.java
@@ -0,0 +1,14 @@
+package stringbuffer;
+
+import java.util.Scanner;
+
+public class intTochar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a=sc.nextFloat();
+        System.out.println("Enter the number"+a);
+        System.out.println(Math.round(a));
+//        char ch=(char) a;
+//        System.out.println("Successfully converted it to char"+ ch);
+    }
+}
